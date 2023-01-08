f14c7b300d9b1ae070f189ecec50031b6e704f27
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-01-08 17:19:43 +0000

    Add zones

---
+++ js/dialogos.js
@@ -0,0 +1,67 @@
+const dialogo0 = {
+    isFala: true,
+    textoFala: "Uuui moina aqui!\n Tá de chuva...",
+    personagem: "ivone"
+};
+
+const dialogo1 = {
+    isFala: true,
+    textoFala: "OH TO MANE ANDA CÁ BAIXO QUE É DESTA QUE VAIS DE FROSQUES",
+    personagem: "ivone"
+};
+
+const dialogo2 = {
+    isFala: false,
+    textoOp1: "Olhe aí o volume senhora",
+    nextOp1: 6,
+    textoOp2: "Por acaso era mesmo com a Ivone que queria falar...",
+    nextOp2: 3,
+    personagem: "user"
+};
+
+const dialogo3 = {
+    isFala: true,
+    textoFala: "oh anda lá desenbucha filho",
+    personagem: "ivone"
+}
+
+const dialogo4 = {
+    isFala: true,
+    textoFala: "OLHA A GINGINHA DA IVONE\n É PÓ TUGA E PÓ CAMONE",
+    personagem: "ivone"
+}
+
+const dialogo5 = {
+    isFala: false,
+    textoOp1: "Olhe aí o volume senhora",
+    nextOp1: 6,
+    textoOp2: "Pode dizer-me onde está o Raul?",
+    nextOp2: 8,
+    personagem: "user"
+}
+
+const dialogo6 = {
+    isFala: true,
+    textoFala: "Olha este armado em carapau de corrida!!",
+    personagem: "ivone"
+}
+
+const dialogo7 = {
+    isFala: true,
+    textoFala: "Senhora ta é no céu! Põente maze a andar daqui oh",
+    personagem: "ivone"
+}
+
+const dialogo8 = {
+    isFala: true,
+    textoFala: "resposta ivone",
+    personagem: "ivone"
+}
+
+const zonaMadragoa = {
+    nome: "Madragoa",
+    dialogos: [dialogo0,dialogo1,dialogo2,dialogo3,dialogo4,dialogo5,dialogo6,dialogo7,dialogo8],
+    imagem: "../img/policia.jfif"
+};
+
+const zonas = [zonaMadragoa];
--- js/main.js
+++ js/main.js
@@ -1,68 +1,5 @@
-
 let numDialogo = 0;
-
-const dialogo0 = {
-    isFala: true,
-    textoFala: "Uuui moina aqui!\n Tá de chuva...",
-    personagem: "ivone"
-};
-
-const dialogo1 = {
-    isFala: true,
-    textoFala: "OH TO MANE ANDA CÁ BAIXO QUE É DESTA QUE VAIS DE FROSQUES",
-    personagem: "ivone"
-};
-
-const dialogo2 = {
-    isFala: false,
-    textoOp1: "Olhe aí o volume senhora",
-    nextOp1: 6,
-    textoOp2: "Por acaso era mesmo com a Ivone que queria falar...",
-    nextOp2: 3,
-    personagem: "user"
-};
-
-const dialogo3 = {
-    isFala: true,
-    textoFala: "oh anda lá desenbucha filho",
-    personagem: "ivone"
-}
-
-const dialogo4 = {
-    isFala: true,
-    textoFala: "OLHA A GINGINHA DA IVONE\n É PÓ TUGA E PÓ CAMONE",
-    personagem: "ivone"
-}
-
-const dialogo5 = {
-    isFala: false,
-    textoOp1: "Olhe aí o volume senhora",
-    nextOp1: 6,
-    textoOp2: "Pode dizer-me onde está o Raul?",
-    nextOp2: 8,
-    personagem: "user"
-}
-
-const dialogo6 = {
-    isFala: true,
-    textoFala: "Olha este armado em carapau de corrida!!",
-    personagem: "ivone"
-}
-
-const dialogo7 = {
-    isFala: true,
-    textoFala: "Senhora ta é no céu! Põente maze a andar daqui oh",
-    personagem: "ivone"
-}
-
-const dialogo8 = {
-    isFala: true,
-    textoFala: "resposta ivone",
-    personagem: "ivone"
-}
-
-
-const dialogos = [dialogo0,dialogo1,dialogo2,dialogo3,dialogo4,dialogo5,dialogo6,dialogo7,dialogo8];
+let numZona = 0;
 
 function showDialog(dialog) {
     if (dialog.isFala) {
@@ -79,20 +16,21 @@ function showDialog(dialog) {
 
 window.onload = function () {
 
-   showDialog(dialogos[numDialogo]);
+    let zonaAtual = zonas[numZona];
+   showDialog(zonaAtual.dialogos[numDialogo]);
 
     document.getElementById("fala").onclick = function () {
-       numDialogo++;
-        showDialog(dialogos[numDialogo]);
+        numDialogo++;
+        showDialog(zonaAtual.dialogos[numDialogo]);
     }
 
     document.getElementById("opcao1").onclick = function () {
-        numDialogo = dialogos[numDialogo].nextOp1;
-        showDialog(dialogos[numDialogo]);
+        numDialogo = zonaAtual.dialogos[numDialogo].nextOp1;
+        showDialog(zonaAtual.dialogos[numDialogo]);
     }
 
     document.getElementById("opcao2").onclick = function () {
-        numDialogo = dialogos[numDialogo].nextOp2;
-        showDialog(dialogos[numDialogo]);
+        numDialogo = zonaAtual.dialogos[numDialogo].nextOp2;
+        showDialog(zonaAtual.dialogos[numDialogo]);
     }
 }
