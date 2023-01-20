5f29a17030068fe47a9b086ce3684539024a00b0
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-01-20 14:50:09 +0000

    Change image path

--- js/desafioImagens.js
+++ js/desafioImagens.js
@@ -4,46 +4,46 @@ let dImgPerguntaAtual = 0;
 
 const desafioImagensConteudo = [
     {
-        imagem1: "url('../img/personagens_des_img/lourenço_1.png')",
-        imagem2: "url('../img/personagens_des_img/lourenço_2.png')",
-        imagem3: "url('../img/personagens_des_img/lourenço_3.png')",
-        imagem4: "url('../img/personagens_des_img/lourenço_4.png')",
+        imagem1: "url('./img/personagens_des_img/lourenço_1.png')",
+        imagem2: "url('./img/personagens_des_img/lourenço_2.png')",
+        imagem3: "url('./img/personagens_des_img/lourenço_3.png')",
+        imagem4: "url('./img/personagens_des_img/lourenço_4.png')",
         op1: "Lourenço",
         op2: "António",
         resposta:1
     },
     {
-        imagem1: "url('../img/personagens_des_img/madalena_1.png')",
-        imagem2: "url('../img/personagens_des_img/madalena_2.png')",
-        imagem3: "url('../img/personagens_des_img/madalena_3.png')",
-        imagem4: "url('../img/personagens_des_img/madalena_4.png')",
+        imagem1: "url('./img/personagens_des_img/madalena_1.png')",
+        imagem2: "url('./img/personagens_des_img/madalena_2.png')",
+        imagem3: "url('./img/personagens_des_img/madalena_3.png')",
+        imagem4: "url('./img/personagens_des_img/madalena_4.png')",
         op1: "Filipa",
         op2: "Madalena",
         resposta:2
     },
     {
-        imagem1: "url('../img/personagens_des_img/mariadapiedade_1.png')",
-        imagem2: "url('../img/personagens_des_img/mariadapiedade_2.png')",
-        imagem3: "url('../img/personagens_des_img/mariadapiedade_3.png')",
-        imagem4: "url('../img/personagens_des_img/mariadapiedade_4.png')",
+        imagem1: "url('./img/personagens_des_img/mariadapiedade_1.png')",
+        imagem2: "url('./img/personagens_des_img/mariadapiedade_2.png')",
+        imagem3: "url('./img/personagens_des_img/mariadapiedade_3.png')",
+        imagem4: "url('./img/personagens_des_img/mariadapiedade_4.png')",
         op1: "Ivone",
         op2: "Maria da Piedade",
         resposta:2
     },
     {
-        imagem1: "url('../img/personagens_des_img/narciso_1.png')",
-        imagem2: "url('../img/personagens_des_img/narciso_2.png')",
-        imagem3: "url('../img/personagens_des_img/narciso_3.png')",
-        imagem4: "url('../img/personagens_des_img/narciso_4.png')",
+        imagem1: "url('./img/personagens_des_img/narciso_1.png')",
+        imagem2: "url('./img/personagens_des_img/narciso_2.png')",
+        imagem3: "url('./img/personagens_des_img/narciso_3.png')",
+        imagem4: "url('./img/personagens_des_img/narciso_4.png')",
         op1: "Narciso",
         op2: "Sebastião",
         resposta:1
     },
     {
-        imagem1: "url('../img/personagens_des_img/padrevito_1.png')",
-        imagem2: "url('../img/personagens_des_img/padrevito_2.png')",
-        imagem3: "url('../img/personagens_des_img/padrevito_3.png')",
-        imagem4: "url('../img/personagens_des_img/padrevito_4.png')",
+        imagem1: "url('./img/personagens_des_img/padrevito_1.png')",
+        imagem2: "url('./img/personagens_des_img/padrevito_2.png')",
+        imagem3: "url('./img/personagens_des_img/padrevito_3.png')",
+        imagem4: "url('./img/personagens_des_img/padrevito_4.png')",
         op1: "Padre Vitó",
         op2: "Padre Serafim",
         resposta:1
