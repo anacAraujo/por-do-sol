a2965c3aae23e2010bf5f7058c4ea52a95a8c309
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2023-01-20 14:45:56 +0000

    Refactor desafio iamgens

--- js/desafioImagens.js
+++ js/desafioImagens.js
@@ -1,192 +1,125 @@
-let numErradas = 0;
-let numCertas = 0;
-let resposta;
+let numErradasDImg = 0;
+let numCertasDImg = 0;
+let dImgPerguntaAtual = 0;
+
+const desafioImagensConteudo = [
+    {
+        imagem1: "url('../img/personagens_des_img/lourenço_1.png')",
+        imagem2: "url('../img/personagens_des_img/lourenço_2.png')",
+        imagem3: "url('../img/personagens_des_img/lourenço_3.png')",
+        imagem4: "url('../img/personagens_des_img/lourenço_4.png')",
+        op1: "Lourenço",
+        op2: "António",
+        resposta:1
+    },
+    {
+        imagem1: "url('../img/personagens_des_img/madalena_1.png')",
+        imagem2: "url('../img/personagens_des_img/madalena_2.png')",
+        imagem3: "url('../img/personagens_des_img/madalena_3.png')",
+        imagem4: "url('../img/personagens_des_img/madalena_4.png')",
+        op1: "Filipa",
+        op2: "Madalena",
+        resposta:2
+    },
+    {
+        imagem1: "url('../img/personagens_des_img/mariadapiedade_1.png')",
+        imagem2: "url('../img/personagens_des_img/mariadapiedade_2.png')",
+        imagem3: "url('../img/personagens_des_img/mariadapiedade_3.png')",
+        imagem4: "url('../img/personagens_des_img/mariadapiedade_4.png')",
+        op1: "Ivone",
+        op2: "Maria da Piedade",
+        resposta:2
+    },
+    {
+        imagem1: "url('../img/personagens_des_img/narciso_1.png')",
+        imagem2: "url('../img/personagens_des_img/narciso_2.png')",
+        imagem3: "url('../img/personagens_des_img/narciso_3.png')",
+        imagem4: "url('../img/personagens_des_img/narciso_4.png')",
+        op1: "Narciso",
+        op2: "Sebastião",
+        resposta:1
+    },
+    {
+        imagem1: "url('../img/personagens_des_img/padrevito_1.png')",
+        imagem2: "url('../img/personagens_des_img/padrevito_2.png')",
+        imagem3: "url('../img/personagens_des_img/padrevito_3.png')",
+        imagem4: "url('../img/personagens_des_img/padrevito_4.png')",
+        op1: "Padre Vitó",
+        op2: "Padre Serafim",
+        resposta:1
+    }
+]
+
+function showDesafioImagens() {
+    document.getElementById("desafioImagens").style.display = "block";
+    document.getElementById("narrativa").style.display = "none";
+    showDImgPergunta();
+}
+
+function showDImgPergunta() {
+    const perguntaAtual = desafioImagensConteudo[dImgPerguntaAtual];
+    document.getElementById("desafioImagens1").style.backgroundImage = perguntaAtual.imagem1;
+    document.getElementById("desafioImagens2").style.backgroundImage = perguntaAtual.imagem2;
+    document.getElementById("desafioImagens3").style.backgroundImage = perguntaAtual.imagem3;
+    document.getElementById("desafioImagens4").style.backgroundImage = perguntaAtual.imagem4;
+    document.getElementById("desafioImagensBotao1").innerHTML = perguntaAtual.op1;
+    document.getElementById("desafioImagensBotao2").innerHTML = perguntaAtual.op2;
+}
+
+function showDImgFinalPerder() {
+    document.getElementById("desafioImagens").style.display = "none";
+    document.getElementById("narrativa").style.display = "block";
+    numZona = 2;
+    numDialogo = 20;
+    zonaAtual = zonas[numZona];
+    document.body.style.backgroundImage = zonaAtual.imagem;
+    mundancaCenario();
+}
+
+function showDImgFinalGanhar() {
+    document.getElementById("desafioImagens").style.display = "none";
+    document.getElementById("narrativa").style.display = "block";
+    numZona = 2;
+    numDialogo = 19;
+    zonaAtual = zonas[numZona];
+    document.body.style.backgroundImage = zonaAtual.imagem;
+    mundancaCenario();
+}
 
 function desafioImgEvents() {
 
-    document.getElementById("desafioImagensBotao1").onclick = function() {
-        desafioImagensRonda1();
+    document.getElementById("desafioImagensBotao1").onclick = function () {
+        const perguntaAtual = desafioImagensConteudo[dImgPerguntaAtual];
+        if (perguntaAtual.resposta === 1) {
+            numCertasDImg++;
+            document.getElementById("desafioImagensRespostaCerta").innerHTML = "Respostas certas: " + numCertasDImg;
+        } else {
+            numErradasDImg++;
+            document.getElementById("desafioImagensRespostaErrada").innerHTML = "Respostas erradas: " + numErradasDImg;
+        }
+        if (numErradasDImg >= 3) {
+            showDImgFinalPerder();
+        } else if (numCertasDImg >= 3) {
+            showDImgFinalGanhar();
+        }
+        dImgPerguntaAtual++;
+        showDImgPergunta();
     }
 
-    document.getElementById("desafioImagensBotao2").onclick = function() {
-        desafioImagensRonda1();
-    }
-
-    document.getElementById("desafioImagensBotao3").onclick = function () {
-        desafioImagensRonda2();
-    }
-
-    document.getElementById("desafioImagensBotao4").onclick = function () {
-        desafioImagensRonda2();
-    }
-
-    document.getElementById("desafioImagensBotao5").onclick = function () {
-        desafioImagensRonda3();
-    }
-
-    document.getElementById("desafioImagensBotao6").onclick = function () {
-        desafioImagensRonda3();
-    }
-
-    document.getElementById("desafioImagensBotao7").onclick = function () {
-        desafioImagensRonda4();
-    }
-
-    document.getElementById("desafioImagensBotao8").onclick = function () {
-        desafioImagensRonda4();
-    }
-
-    document.getElementById("desafioImagensBotao9").onclick = function () {
-        desafioImagensRonda5();
-
-    }
-
-    document.getElementById("desafioImagensBotao10").onclick = function () {
-        desafioImagensRonda5();
-    }
-}
-
-function showDesafioImagens() {
-
-    document.getElementById("desafioImagens").style.display = "block";
-    document.getElementById("narrativa").style.display = "none";
-
-    document.getElementById("desafioImagens5").style.display = "none";
-    document.getElementById("desafioImagens6").style.display = "none";
-    document.getElementById("desafioImagens7").style.display = "none";
-    document.getElementById("desafioImagens8").style.display = "none";
-    document.getElementById("desafioImagens9").style.display = "none";
-    document.getElementById("desafioImagens10").style.display = "none";
-    document.getElementById("desafioImagens11").style.display = "none";
-    document.getElementById("desafioImagens12").style.display = "none";
-    document.getElementById("desafioImagens13").style.display = "none";
-    document.getElementById("desafioImagens14").style.display = "none";
-    document.getElementById("desafioImagens15").style.display = "none";
-    document.getElementById("desafioImagens16").style.display = "none";
-    document.getElementById("desafioImagens17").style.display = "none";
-    document.getElementById("desafioImagens18").style.display = "none";
-    document.getElementById("desafioImagens19").style.display = "none";
-    document.getElementById("desafioImagens20").style.display = "none";
-    document.getElementById("desafioImagensBotao3").style.display = "none";
-    document.getElementById("desafioImagensBotao4").style.display = "none";
-    document.getElementById("desafioImagensBotao5").style.display = "none";
-    document.getElementById("desafioImagensBotao6").style.display = "none";
-    document.getElementById("desafioImagensBotao7").style.display = "none";
-    document.getElementById("desafioImagensBotao8").style.display = "none";
-    document.getElementById("desafioImagensBotao9").style.display = "none";
-    document.getElementById("desafioImagensBotao10").style.display = "none";
-
-
-
-    if (numErradas >= 3 ){
-        document.getElementById("desafioImagens").style.display = "none";
-        document.getElementById("narrativa").style.display = "block";
-    }
-
-}
-
-function desafioImagensRonda1() {
-    document.getElementById("desafioImagens1").style.display = "none";
-    document.getElementById("desafioImagens2").style.display = "none";
-    document.getElementById("desafioImagens3").style.display = "none";
-    document.getElementById("desafioImagens4").style.display = "none";
-    document.getElementById("desafioImagens5").style.display = "block";
-    document.getElementById("desafioImagens6").style.display = "block";
-    document.getElementById("desafioImagens7").style.display = "block";
-    document.getElementById("desafioImagens8").style.display = "block";
-
-    document.getElementById("desafioImagensBotao1").style.display = "none";
-    document.getElementById("desafioImagensBotao2").style.display = "none";
-    document.getElementById("desafioImagensBotao3").style.display = "inline";
-    document.getElementById("desafioImagensBotao4").style.display = "inline";
-
-    if (resposta === "Lourenço") {
-        numCertas++;
-        document.getElementById("desafioImagensRespostaCerta").innerHTML = "Respostas Certas: " + numCertas;
-    } else {
-        numErradas++;
-        document.getElementById("desafioImagensRespostaErrada").innerHTML = "Respostas Erradas: " + numErradas;
-    }
-}
-
-function desafioImagensRonda2() {
-    document.getElementById("desafioImagens5").style.display = "none";
-    document.getElementById("desafioImagens6").style.display = "none";
-    document.getElementById("desafioImagens7").style.display = "none";
-    document.getElementById("desafioImagens8").style.display = "none";
-    document.getElementById("desafioImagens9").style.display = "block";
-    document.getElementById("desafioImagens10").style.display = "block";
-    document.getElementById("desafioImagens11").style.display = "block";
-    document.getElementById("desafioImagens12").style.display = "block";
-
-    document.getElementById("desafioImagensBotao3").style.display = "none";
-    document.getElementById("desafioImagensBotao4").style.display = "none";
-    document.getElementById("desafioImagensBotao5").style.display = "inline";
-    document.getElementById("desafioImagensBotao6").style.display = "inline";
-
-    if (resposta === "Madalena") {
-        numCertas++;
-        document.getElementById("desafioImagensRespostaCerta").innerHTML = "Respostas Certas: " + numCertas;
-    } else {
-        numErradas++;
-        document.getElementById("desafioImagensRespostaErrada").innerHTML = "Respostas Erradas: " + numErradas;
-    }
-}
-
-function desafioImagensRonda3() {
-    document.getElementById("desafioImagens9").style.display = "none";
-    document.getElementById("desafioImagens10").style.display = "none";
-    document.getElementById("desafioImagens11").style.display = "none";
-    document.getElementById("desafioImagens12").style.display = "none";
-    document.getElementById("desafioImagens13").style.display = "block";
-    document.getElementById("desafioImagens14").style.display = "block";
-    document.getElementById("desafioImagens15").style.display = "block";
-    document.getElementById("desafioImagens16").style.display = "block";
-
-    document.getElementById("desafioImagensBotao5").style.display = "none";
-    document.getElementById("desafioImagensBotao6").style.display = "none";
-    document.getElementById("desafioImagensBotao7").style.display = "inline";
-    document.getElementById("desafioImagensBotao8").style.display = "inline";
-
-    if (resposta === "Maria da Piedade") {
-        numCertas++;
-        document.getElementById("desafioImagensRespostaCerta").innerHTML = "Respostas Certas: " + numCertas;
-    } else {
-        numErradas++;
-        document.getElementById("desafioImagensRespostaErrada").innerHTML = "Respostas Erradas: " + numErradas;
-    }
-}
-
-function desafioImagensRonda4() {
-    document.getElementById("desafioImagens13").style.display = "none";
-    document.getElementById("desafioImagens14").style.display = "none";
-    document.getElementById("desafioImagens15").style.display = "none";
-    document.getElementById("desafioImagens16").style.display = "none";
-    document.getElementById("desafioImagens17").style.display = "block";
-    document.getElementById("desafioImagens18").style.display = "block";
-    document.getElementById("desafioImagens19").style.display = "block";
-    document.getElementById("desafioImagens20").style.display = "block";
-
-    document.getElementById("desafioImagensBotao7").style.display = "none";
-    document.getElementById("desafioImagensBotao8").style.display = "none";
-    document.getElementById("desafioImagensBotao9").style.display = "inline";
-    document.getElementById("desafioImagensBotao10").style.display = "inline";
-
-    if (resposta === "Narciso") {
-        numCertas++;
-        document.getElementById("desafioImagensRespostaCerta").innerHTML = "Respostas Certas: " + numCertas;
-    } else {
-        numErradas++;
-        document.getElementById("desafioImagensRespostaErrada").innerHTML = "Respostas Erradas: " + numErradas;
-    }
-
-}
-function desafioImagensRonda5() {
-    if (resposta === "Padre Vitó") {
-        numCertas++;
-        document.getElementById("desafioImagensRespostaCerta").innerHTML = "Respostas Certas: " + numCertas;
-    } else {
-        numErradas++;
-        document.getElementById("desafioImagensRespostaErrada").innerHTML = "Respostas Erradas: " + numErradas;
+    document.getElementById("desafioImagensBotao2").onclick = function () {
+        if (perguntaAtual.resposta === 2) {
+            numCertasDImg++;
+            document.getElementById("desafioImagensRespostaCerta").innerHTML = "Respostas certas: " + numCertasDImg;
+        } else {
+            numErradasDImg++;
+            document.getElementById("desafioImagensRespostaErrada").innerHTML = "Respostas erradas: " + numErradasDImg;
+        }
+        if (numErradasDImg >= 3) {
+            showDImgFinalPerder();
+        } else if (numCertasDImg >= 3) {
+            showDImgFinalGanhar();
+        }
+        dImgPerguntaAtual++;
+        showDImgPergunta();
     }
 }
